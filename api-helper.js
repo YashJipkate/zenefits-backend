fb59a759af3cdab26dea996656e62ee673dbecfe
YashJipkate <yashjipkate@gmail.com>
2020-03-17 03:52:17 +0530

    Fix null return bug

--- api-helper.js
+++ api-helper.js
@@ -1,17 +1,17 @@
 const request = require('request');
 
 module.exports = {
-    makeApiCall: (url, startingAfter) => {
-        const queryString = ((startingAfter==='null' || typeof startingAfter==='undefined') ? '' : '?startingAfter=' + startingAfter)
+    makeApiCall = (url, startingAfter) => {
+        const queryString = ((startingAfter==='null' || typeof startingAfter==='undefined') ? '' : '?starting_after=' + startingAfter);
         return new Promise((resolve, reject) => {
             request({
                 url: url + queryString,
                 headers: {Authorization: 'Bearer ' + process.env.AUTH_TOKEN}},
                 (err, res, body) => {
-                    if (err) { reject(err) }
-                    resolve(body)
+                    if (err) { reject(err); }
+                    resolve(body);
                 }
-            )
-        })
+            );
+        });
     }
-}
+};
