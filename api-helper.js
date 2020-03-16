4d00922ad8df997cac1374a033f12d6e33e86f21
YashJipkate <yashjipkate@gmail.com>
2020-03-17 03:54:59 +0530

    Fix cors issue

--- api-helper.js
+++ api-helper.js
@@ -1,15 +1,15 @@
-const request = require('request');
+const request = require('request')
 
 module.exports = {
     makeApiCall = (url, startingAfter) => {
-        const queryString = ((startingAfter==='null' || typeof startingAfter==='undefined') ? '' : '?starting_after=' + startingAfter);
+        const queryString = ((startingAfter==='null' || typeof startingAfter==='undefined') ? '' : '?starting_after=' + startingAfter)
         return new Promise((resolve, reject) => {
             request({
                 url: url + queryString,
                 headers: {Authorization: 'Bearer ' + process.env.AUTH_TOKEN}},
                 (err, res, body) => {
-                    if (err) { reject(err); }
-                    resolve(body);
+                    if (err) { reject(err) }
+                    resolve(body)
                 }
             );
         });
